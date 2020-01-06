88c2ade0387805ae81b653d1ab913d248df83a8c
Aditya Singh <samant04aditya@gmail.com>
2020-01-06 19:02:17 +0530

    Create mail.py

---
+++ sending-email/mail.py
@@ -0,0 +1,20 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path 
+
+html = Template(Path('index.html').read_text())
+email = EmailMessage()
+email['from'] = 'Aditya Singh'
+email['to'] = '<to email address>'
+email['subject'] = 'Any Subject'
+
+email.set_content(html.substitute({'name': 'sender'}), 'html')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+  smtp.ehlo()
+  smtp.starttls()
+  smtp.login('<your email address>', '<your password>')
+  smtp.send_message(email)
+
+print('Email Sent!')
