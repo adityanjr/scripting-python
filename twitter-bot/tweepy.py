f4a53e8b84c02490ac9f0271221a0c1b351bd45a
Aditya Singh <samant04aditya@gmail.com>
2020-01-09 23:26:52 +0530

    Create tweepy.py

---
+++ twitter-bot/tweepy.py
@@ -0,0 +1,41 @@
+import tweepy
+import time
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+user = api.me()
+print (user.name) #prints the name
+print (user.screen_name)
+print (user.followers_count)
+
+search = "any keyword"
+numberOfTweets = 2
+
+def limit_handle(cursor):
+  while True:
+    try:
+      yield cursor.next()
+    except tweepy.RateLimitError:
+      time.sleep(1000)
+
+#Follow Back your followers!
+for follower in limit_handle(tweepy.Cursor(api.followers).items()):
+  if follower.name == 'Usernamehere':
+    print(follower.name)
+    follower.follow()
+
+#Retweet anything with a keyword!
+for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
+    try:
+        tweet.favorite()
+        print('Retweeted the tweet')
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
